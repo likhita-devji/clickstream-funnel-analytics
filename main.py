ae80a7b903db2044d4df8f490b41f799da5aef53
LikhitaDevji <likhitadevji9@gmail.com>
2026-06-26 15:05:46 +0530

    Initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+print("=" * 50)
+print("Predictive Clickstream Analytics Project")
+print("=" * 50)
+
+print("Pandas Version:", pd.__version__)
+print("NumPy Version:", np.__version__)
+
+print("\nEnvironment Setup Successful!")
